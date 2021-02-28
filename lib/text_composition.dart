327a31fa27cfef0ca6f6efc6c6ba596d6c4454a9
mabdc <747455334@qq.com>
2021-02-28 12:25:13 +0800

    已完成 TODO example 测试

--- lib/text_composition.dart
+++ lib/text_composition.dart
@@ -2,6 +2,7 @@ library text_composition;
 
 import 'package:flutter/material.dart';
 
+/// * 暂不支持图片
 /// * 文本排版
 /// * 两端对齐
 /// * 底栏对齐
@@ -12,13 +13,20 @@ class TextComposition {
 
   /// 待渲染文本内容
   /// 已经预处理: 不重新计算空行 不重新缩进
-  final List<String>? paragraphs;
+  late final List<String> _paragraphs;
+  List<String> get paragraphs => _paragraphs;
 
-  /// 页面宽度
-  final double screenWidth;
+  /// 容器宽度
+  final double boxWidth;
 
-  /// 页面高度
-  final double screenHeight;
+  /// 对齐宽度判断
+  late final double _boxWidth;
+
+  /// 容器高度
+  final double boxHeight;
+
+  /// 对齐高度判断
+  late final double _boxHeight;
 
   /// 字号
   final double size;
@@ -29,6 +37,9 @@ class TextComposition {
   /// 行高
   final double height;
 
+  /// 实际行高
+  late final double _height;
+
   /// 段高
   final double paragraph;
 
@@ -36,13 +47,14 @@ class TextComposition {
   final bool shouldJustifyHeight;
 
   /// 每一页内容
-  late List<TextPage> _pages;
+  late final List<TextPage> _pages;
   List<TextPage> get pages => _pages;
+  int get pageCount => _pages.length;
 
   /// 每一页内容
-  late List<TextLine> _lines;
+  late final List<TextLine> _lines;
   List<TextLine> get lines => _lines;
-  int get pageCount => _pages.length;
+  int get lineCount => _lines.length;
 
   /// * 文本排版
   /// * 两端对齐
@@ -55,65 +67,93 @@ class TextComposition {
   /// * [size] 字号
   /// * [height] 行高
   /// * [paragraph] 段高
-  /// * [screenWidth] 页面宽度
-  /// * [screenHeight] 页面高度
+  /// * [boxWidth] 容器宽度
+  /// * [boxHeight] 容器高度
   /// * [isHeightJustify] 是否底栏对齐
   TextComposition({
     this.text,
-    this.paragraphs,
+    List<String>? paragraphs,
     required this.size,
     this.height = 1,
     this.family,
     required this.paragraph,
-    required this.screenWidth,
-    required this.screenHeight,
+    required this.boxWidth,
+    required this.boxHeight,
     this.shouldJustifyHeight = true,
   }) {
-    /// [ts] 只有一行的`TextPainter` [offset] 只有一行的`offset`
-    final tp = TextPainter(textDirection: TextDirection.ltr, maxLines: 1);
-    final offset = Offset(screenWidth, 1);
-    final style = TextStyle(fontSize: size, fontFamily: family);
-
-    // TODO 分页
+    _height = size * height;
+    _boxWidth = boxWidth - size;
+    _boxHeight = boxHeight - _height;
+    // 下一行是新段落时
+    final _boxHeight2 = boxHeight - _height - paragraph;
+    _paragraphs = paragraphs ?? text?.split("\n") ?? <String>[];
     _pages = <TextPage>[];
     _lines = <TextLine>[];
-    var paragraphFirstLine = true;
-    final lineHeight = height * size;
+
+    /// [ts] 只有一行的`TextPainter` [offset] 只有一行的`offset`
+    final tp = TextPainter(textDirection: TextDirection.ltr, maxLines: 1);
+    final offset = Offset(boxWidth, 1);
+    final style = TextStyle(fontSize: size, fontFamily: family);
+
+    var paragraphCount = 0;
     var pageHeight = 0.0;
-    for (var p in paragraphs ?? text?.split("\n") ?? <String>[]) {
-      if (paragraphFirstLine) lines.add(TextLine(paragraphFirstLine: true));
-      if (p.isEmpty) {
-        lines.add(TextLine());
-      } else
-        while (p.isNotEmpty) {
-          tp.text = TextSpan(text: p, style: style);
-          tp.layout(maxWidth: screenWidth);
-          final textCount = tp.getPositionForOffset(offset).offset;
-          lines.add(TextLine(
-              text: p.substring(0, textCount), textCount: textCount, width: tp.width));
-          p = p.substring(textCount);
-          pageHeight += lineHeight;
-        }
-      paragraphFirstLine = true;
-      pageHeight += paragraph;
+    var endLine = 0;
+    var startLine = endLine;
+
+    /// 下一页
+    void newPage() {
+      _pages.add(TextPage(startLine, endLine, pageHeight, paragraphCount));
+      paragraphCount = 0;
+      pageHeight = 0.0;
+      startLine = endLine;
     }
-    _pages.add(TextPage(0, _lines.length, pageHeight));
+
+    for (var p in _paragraphs) {
+      while (true) {
+        /// 判断分页 依据: `_boxHeight` `_boxHeight2`是否可以容纳下一行
+        /// 段落结束 跳出循环
+        if (p.isEmpty) {
+          if (pageHeight > _boxHeight2) {
+            newPage();
+          } else {
+            lines.add(TextLine(paragraphGap: true));
+            pageHeight += paragraph;
+            paragraphCount++;
+            endLine++;
+          }
+          break;
+        } else if (pageHeight > _boxHeight) {
+          newPage();
+        }
+        tp.text = TextSpan(text: p, style: style);
+        tp.layout(maxWidth: boxWidth);
+        final textCount = tp.getPositionForOffset(offset).offset;
+        lines.add(TextLine(
+            text: p.substring(0, textCount), textCount: textCount, width: tp.width));
+        endLine++;
+        pageHeight += _height;
+        p = p.substring(textCount);
+      }
+    }
+    if (endLine > startLine) {
+      _pages.add(TextPage(startLine, endLine, pageHeight, paragraphCount));
+    }
   }
 
   TextSpan getLineView(TextLine line) {
     if (line.textCount == 0) return TextSpan(text: "");
-    if (screenWidth - line.width < size) return TextSpan(text: line.text);
+    if (_boxWidth > line.width) return TextSpan(text: line.text);
     return TextSpan(
       text: line.text,
       style: TextStyle(
-        wordSpacing: (screenWidth - line.width) / line.textCount,
+        wordSpacing: (boxWidth - line.width) / line.textCount,
       ),
     );
   }
 
   TextSpan getPageView(TextPage page) {
-    final paragraphHeight = screenHeight - page.height < paragraph
-        ? (screenHeight - page.height) / (page.endLine - page.endLine)
+    final paragraphHeight = shouldJustifyHeight && page.height > _boxHeight
+        ? (boxHeight - page.height) / page.paragraphCount
         : 1.0;
     return TextSpan(
       style: TextStyle(
@@ -122,7 +162,7 @@ class TextComposition {
         fontFamily: family,
       ),
       children: lines.sublist(page.startLine, page.endLine).map((line) {
-        if (line.paragraphFirstLine)
+        if (line.paragraphGap)
           return TextSpan(
             text: "",
             style: TextStyle(
@@ -139,19 +179,20 @@ class TextComposition {
 class TextPage {
   final int startLine;
   final int endLine;
+  final int paragraphCount;
   final double height;
-  TextPage(this.startLine, this.endLine, this.height);
+  TextPage(this.startLine, this.endLine, this.height, this.paragraphCount);
 }
 
 class TextLine {
   final String text;
   final int textCount;
   final double width;
-  final bool paragraphFirstLine;
+  final bool paragraphGap;
   TextLine({
     this.text = "",
     this.textCount = 0,
     this.width = 0,
-    this.paragraphFirstLine = false,
+    this.paragraphGap = false,
   });
 }
