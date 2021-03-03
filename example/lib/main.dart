ec2ce30e8a84d205903f03b38f854bed96a379f2
mabdc <747455334@qq.com>
2021-03-03 14:06:51 +0800

    优化；兼容 非等高 非等宽 字体

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,10 +6,11 @@ import 'package:text_composition/text_composition.dart';
 
 import 'first_chapter.dart';
 
-
 ///
-/// 调试时修改162行查看详细的布局输出和绘图方式
-/// `tc.getPageWidget(tc.pages[index], true, true/false),`
+/// 调试时修改161行
+/// * [useCanvas] 绘图方式（canvas/richText） [debug] 查看详细的布局输出
+/// * 等宽字体 时 `richText`和`canvas`结果一致
+/// * 非等宽字体 使用`canvas`才能两端对齐 [useCanvas] 应设为 [true]
 ///
 main(List<String> args) {
   runApp(MaterialApp(home: Setting()));
@@ -23,7 +24,10 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> {
-  var size = TextEditingController(text: '16'),
+  var pwidth = window.physicalSize.width,
+      pheight = window.physicalSize.height,
+      ratio = window.devicePixelRatio,
+      size = TextEditingController(text: '16'),
       height = TextEditingController(text: '1.55'),
       paragraph = TextEditingController(text: '10'),
       shouldJustifyHeight = true,
@@ -32,19 +36,12 @@ class _SettingState extends State<Setting> {
   TextComposition? tc;
   @override
   Widget build(BuildContext context) {
-    final pwidth = window.physicalSize.width;
-    final pheight = window.physicalSize.height;
-    final ratio = window.devicePixelRatio;
     return Scaffold(
       body: ListView(
         padding: EdgeInsets.all(30),
         children: [
           Text("width / ratio: $pwidth / $ratio = ${pwidth / ratio}"),
           Text("height / ratio: $pheight / $ratio = ${pheight / ratio}"),
-          OutlinedButton(
-            child: Text("重新获取屏幕大小"),
-            onPressed: () => setState(() {}),
-          ),
           TextField(
             decoration: InputDecoration(labelText: "字号 size"),
             controller: size,
@@ -73,11 +70,13 @@ class _SettingState extends State<Setting> {
           OutlinedButton(
             child: Text("开始或重新排版"),
             onPressed: () {
+              pwidth = window.physicalSize.width;
+              pheight = window.physicalSize.height;
+              ratio = window.devicePixelRatio;
               start = DateTime.now();
               tc = TextComposition(
                 paragraphs: first_chapter,
                 style: TextStyle(
-                  fontFamily: "jiali",
                   fontSize: double.tryParse(size.text),
                   height: double.tryParse(height.text),
                 ),
@@ -88,8 +87,7 @@ class _SettingState extends State<Setting> {
                 linkPattern: "<img",
                 linkText: (s) =>
                     RegExp('src=".*/([^/]+)"').firstMatch(s)?.group(1) ?? "链接",
-                linkStyle: TextStyle(
-                    color: Colors.cyan, fontStyle: FontStyle.italic, fontFamily: "jiali"),
+                linkStyle: TextStyle(color: Colors.cyan, fontStyle: FontStyle.italic),
                 onLinkTap: (s) => Navigator.of(context).push(MaterialPageRoute(
                     builder: (BuildContext context) => Scaffold(
                           appBar: AppBar(),
@@ -144,22 +142,23 @@ class Page extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Column(
-                    children: List.generate(
-                  tc.boxHeight ~/ 10 + 2,
-                  (index) => Container(
-                    height: 10,
-                    width: 20,
-                    color: index % 2 == 0 ?null : Colors.cyan ,
-                    child: Text(
-                      index % 2 == 0 ? "" : ((index + 1) * 10).toString() ,
-                      textAlign: TextAlign.end,
-                      style: TextStyle(color: Colors.white, fontSize: 8),
+                  children: List.generate(
+                    tc.boxHeight ~/ 10 + 2,
+                    (index) => Container(
+                      height: 10,
+                      width: 20,
+                      color: index % 2 == 0 ? null : Colors.cyan,
+                      child: Text(
+                        index % 2 == 0 ? "" : ((index + 1) * 10).toString(),
+                        textAlign: TextAlign.end,
+                        style: TextStyle(color: Colors.white, fontSize: 10, height: 1),
+                      ),
                     ),
                   ),
-                )),
+                ),
                 Column(
                   children: [
-                    tc.getPageWidget(tc.pages[index], false, false),
+                    tc.getPageWidget(tc.pages[index], true, false),
                     Container(
                       height: 20,
                       width: tc.boxWidth,
--- lib/text_composition.dart
+++ lib/text_composition.dart
@@ -23,7 +23,7 @@ class TextComposition {
   /// 容器高度
   final double boxHeight;
 
-  /// 字体样式 字号 行高 字体 字色
+  /// 字体样式 字号 [size] 行高 [height] 字体 [family] 字色[Color]
   final TextStyle? style;
 
   /// 段间距
@@ -55,13 +55,10 @@ class TextComposition {
   /// * [text] 待渲染文本内容 已经预处理: 不重新计算空行 不重新缩进
   /// * [paragraphs] 待渲染文本内容 已经预处理: 不重新计算空行 不重新缩进
   /// * [paragraphs] 为空时使用[text], 否则忽略[text],
-  /// * [size] 字号
-  /// * [style] 字体样式 字号 行高 字体 字色
-  /// * [height] 行高
-  /// * [family] 字体
+  /// * [style] 字体样式 字号 [size] 行高 [height] 字体 [family] 字色[Color]
   /// * [boxWidth] 容器宽度
   /// * [boxHeight] 容器高度
-  /// * [paragraph] 段间距
+  /// * [paragraph] 段间距 对齐到整数像素
   /// * [shouldJustifyHeight] 是否底栏对齐
   TextComposition({
     List<String>? paragraphs,
@@ -84,19 +81,14 @@ class TextComposition {
     final tp = TextPainter(textDirection: TextDirection.ltr, maxLines: 1);
     final offset = Offset(boxWidth, 1);
     final size = style?.fontSize ?? 14;
-    final _height = (TextPainter(
-      text: TextSpan(text: "高度", style: style),
-      textDirection: TextDirection.ltr,
-      maxLines: 1,
-    )..layout())
-        .height
-        .toInt();
-    final _boxHeight = boxHeight - _height;
-    final _boxHeight2 = _boxHeight - paragraph;
+    // [_boxWidth] 仅用于判断段尾是否需要调整 [size] 准确性不重要
     final _boxWidth = boxWidth - size;
+    // [_boxHeight] 仅用作判断容纳下一行依据 [_height] 是否实际行高不重要
+    final _boxHeight = boxHeight - size * (style?.height ?? 1.0);
+    final _boxHeight2 = _boxHeight - paragraph;
 
     var paragraphCount = 0;
-    var pageHeight = 0;
+    var pageHeight = 0.0;
     var startLine = 0;
 
     /// 下一页 判断分页 依据: `_boxHeight` `_boxHeight2`是否可以容纳下一行
@@ -120,27 +112,23 @@ class TextComposition {
 
     for (var p in _paragraphs) {
       if (linkPattern != null && p.startsWith(linkPattern!)) {
-        pageHeight += _height;
+        pageHeight += tp.height;
         lines.add(TextLine(text: p, link: true));
         newParagraph();
       } else
         while (true) {
-          pageHeight += _height;
           tp.text = TextSpan(text: p, style: style);
           tp.layout(maxWidth: boxWidth);
+          pageHeight += tp.height;
           final textCount = tp.getPositionForOffset(offset).offset;
           if (p.length == textCount) {
             lines.add(TextLine(
-                text: p,
-                textCount: textCount,
-                width: tp.width,
-                shouldJustifyWidth: tp.width > _boxWidth));
+                text: p, width: tp.width, shouldJustifyWidth: tp.width > _boxWidth));
             newParagraph();
             break;
           } else {
             lines.add(TextLine(
                 text: p.substring(0, textCount),
-                textCount: textCount,
                 width: tp.width,
                 shouldJustifyWidth: true));
             p = p.substring(textCount);
@@ -156,14 +144,14 @@ class TextComposition {
   }
 
   TextSpan getLineView(TextLine line, TextPainter tp) {
-    if (line.textCount == 0) return TextSpan(text: "\n \n");
+    if (line.text.isEmpty) return TextSpan(text: "\n \n");
     if (line.shouldJustifyWidth) {
       tp.text = TextSpan(text: line.text, style: style);
       tp.layout();
       return TextSpan(
         text: line.text,
         style: TextStyle(
-          letterSpacing: (boxWidth - 1 - tp.width) / line.textCount,
+          letterSpacing: (boxWidth - 1 - tp.width) / line.text.length,
         ),
       );
     }
@@ -174,7 +162,7 @@ class TextComposition {
     var paragraphJustifyHeight = paragraph.toDouble();
     var restJustifyHeight = 0;
     if (shouldJustifyHeight && page.shouldJustifyHeight) {
-      final rest = boxHeight.ceil() - page.height;
+      final rest = boxHeight.floor() - page.height.floor();
       restJustifyHeight = rest % page.paragraphCount;
       paragraphJustifyHeight += rest ~/ page.paragraphCount;
     }
@@ -210,49 +198,30 @@ class TextComposition {
     }).toList();
   }
 
-  Widget getPageWidget(TextPage page, [bool debug = false, bool useCanvas = false]) {
+  /// * [useCanvas] 绘图方式（canvas/richText） [debug] 查看详细的布局输出
+  /// * 等宽字体 时 `richText`和`canvas`结果一致
+  /// * 非等宽字体 使用`canvas`才能两端对齐 [useCanvas] 应设为 [true] 
+  Widget getPageWidget(TextPage page, [bool useCanvas = true, bool debug = false]) {
+    print("****** [TextComposition page start] [${DateTime.now()}] ******");
     final ts = TextSpan(style: style, children: getPageSpans(page));
     
     if (debug) {
-      var paragraphJustifyHeight = paragraph;
-      var restJustifyHeight = 0;
-      if (shouldJustifyHeight && page.shouldJustifyHeight) {
-        final rest = boxHeight.ceil() - page.height;
-        restJustifyHeight = rest % page.paragraphCount;
-        paragraphJustifyHeight += rest ~/ page.paragraphCount;
-      }
-      print("****** 一页开始 ******");
-      print("序号 预期 实际 内容");
       final tp = TextPainter(text: ts, textDirection: TextDirection.ltr);
-      var pageHeight = 0;
-      for (var i = page.startLine; i < page.endLine; i++) {
-        tp.maxLines = i + 1;
+      var lastLineHeight = 0;
+      print("序号 行高 累计 宽度 内容");
+      for (var i = 1, end = page.endLine - page.startLine + 1; i < end; i++) {
+        tp.maxLines = i;
         tp.layout(maxWidth: boxWidth);
-        final line = lines[i];
-        final height = (TextPainter(
-          text: TextSpan(text: "高度", style: style),
-          textDirection: TextDirection.ltr,
-          maxLines: 1,
-        )..layout())
-            .height
-            .round();
-        if (line.paragraphGap) {
-          if (restJustifyHeight-- > 0) {
-            pageHeight += paragraphJustifyHeight + 1;
-          } else {
-            pageHeight += paragraphJustifyHeight;
-          }
-        } else {
-          pageHeight += height;
-        }
-        print("$i $pageHeight ${tp.height.toInt()} ${line.text}");
+        final height = tp.height.toInt();
+        print(
+            "$i ${height - lastLineHeight} $height ${tp.width.toInt()} ${lines[i + page.startLine - 1].text}");
+        lastLineHeight = height;
       }
-      print("****** 一页结束 ******");
     }
+    print("****** [TextComposition page end  ] [${DateTime.now()}] ******");
 
     if (useCanvas) {
       final tp = TextPainter(text: ts, textDirection: TextDirection.ltr);
-      tp.maxLines = null;
       tp.layout(maxWidth: boxWidth);
       return Container(
         width: boxWidth,
@@ -288,7 +257,7 @@ class TextPage {
   final int startLine;
   final int endLine;
   final int paragraphCount;
-  final int height;
+  final double height;
   final bool shouldJustifyHeight;
   TextPage(this.startLine, this.endLine, this.height, this.paragraphCount,
       [this.shouldJustifyHeight = true]);
@@ -296,14 +265,12 @@ class TextPage {
 
 class TextLine {
   final String text;
-  final int textCount;
   final double width;
   final bool paragraphGap;
   final bool shouldJustifyWidth;
   final bool link;
   TextLine({
     this.text = "",
-    this.textCount = 0,
     this.width = 0,
     this.link = false,
     this.paragraphGap = false,
